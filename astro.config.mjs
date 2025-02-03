7abc07fb0e8f4bb986585053b14b2f6357c29e58
oeaksoy <oeaksoy@yahoo.com>
2025-02-03 01:52:18 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://oeaksoy.github.io/galuxea/",
-  base: "/energy-template"
+  site: "https://oeaksoy.github.io",
+  base: "/galuxea"
 });
